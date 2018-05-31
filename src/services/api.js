2b45500e4742cd8db54fd57a331e3396e29c3b37
unknown <force-special@hotmail.com>
2018-05-31 11:22:52 +0000

    adding cd

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
+let baseURL
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    baseURL = 'http://localhost:1337'
+} else if (process.env.NODE_ENV === 'production') {
+    baseURL = 'http://backend.tech.sdrm.me:7331'
+} else if (process.env.NODE_ENV === 'ci') {
+    baseURL = 'http://app-sails:1337'
+}
+
 export default {
     customApi(method, url) {
         return axios.create({
-            baseURL: `http://localhost:1337`
+            baseURL: baseURL
         })[method](url)
     },
 
     customApiParam(method, url, obj) {
         return axios.create({
-            baseURL: `http://localhost:1337`
+            baseURL: baseURL
         })[method](url, obj)
     }
 }
